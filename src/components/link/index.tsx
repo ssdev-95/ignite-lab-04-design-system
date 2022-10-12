551cc770bedccfba0f92eedbc15517622eb4aa5b
Salomao S <souza95salomao@gmail.com>
2022-10-12 20:37:27 +0000

    add: link component and related story

---
+++ src/components/link/index.tsx
@@ -0,0 +1,12 @@
+import { AnchorHTMLAttributes } from 'react'
+export interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+export function Link(props:LinkProps) {
+  return (
+		<a
+			className="text-md text-green-400 hover:text-green-300 font-semibold"
+			target="_blank"
+			{...props}
+		/>
+	)
+}
---
+++ src/components/link/link.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Link, LinkProps } from '.'
+
+export default {
+  title: 'Components/Link',
+  component: Link,
+	args: {
+	  href: '#',
+		children: 'Salame world'
+	},
+	argTypes: {}
+} as Meta<LinkProps>
+
+export const Default:StoryObj<LinkProps> = {}
