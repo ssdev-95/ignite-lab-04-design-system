455c99b0baad05b4d71c13a45f5f39d419754c54
Salomao S <souza95salomao@gmail.com>
2022-10-12 20:18:10 +0000

    add: checkbox component and related story

---
+++ src/components/checkbox/checkbox.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Checkbox, CheckboxProps } from '.'
+
+export default {
+  title: 'Components/Checkbox',
+	component: Checkbox,
+	args: {
+	  children: 'Salame é bom?'
+	},
+	argTypes: {
+	  children: {
+		  table: {
+			  disable: true
+			}
+		}
+	}
+} as Meta<CheckboxProps>
+
+export const Default:StoryObj<CheckboxProps> = {}
---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,25 @@
+import * as RCheckbox from '@radix-ui/react-checkbox'
+import { Check } from 'phosphor-react'
+
+import { Text } from '../text'
+
+export interface CheckboxProps {
+  children?: string
+}
+
+export function Checkbox({ children }:CheckboxProps) {
+  return (
+	  <label className="flex items-center gap-2">
+  	  <RCheckbox.Root className="bg-gray-800 h-6 w-6 p-1/2">
+			  <RCheckbox.CheckboxIndicator>
+				  <Check
+					  weight="bold"
+						className="h-5 w-5 text-green-400"
+					/>
+				</RCheckbox.CheckboxIndicator>
+	  	</RCheckbox.Root>
+
+			<Text size="small">{children}</Text>
+		</label>
+	)
+}
