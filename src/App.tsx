5a4a397230613c4818626defedfa7fe45a05ae98
Salomao S <souza95salomao@gmail.com>
2022-10-12 20:38:39 +0000

    update: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/header'
 import { Form } from './components/form'
+import { Footer } from './components/footer'
 
 export function App() {
   return (
 	  <>
 		  <Header />
 			<Form />
+			<Footer />
 		</>
 	)
 }
